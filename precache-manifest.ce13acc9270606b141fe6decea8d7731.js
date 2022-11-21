4efd050c52a433d8869e6659afb0febc06aded48
Fatimah ALhamawi <68357777+Fatimah281@users.noreply.github.com>
2022-11-21 10:26:31 +0300

    test2

---
+++ precache-manifest.ce13acc9270606b141fe6decea8d7731.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8f504a214b6f180abf6",
+    "url": "/static/css/main.99898e6b.chunk.css"
+  },
+  {
+    "revision": "a8f504a214b6f180abf6",
+    "url": "/static/js/main.6bc6704b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c4d89c27f5e47483c479",
+    "url": "/static/js/2.f91f1ae0.chunk.js"
+  },
+  {
+    "revision": "2226eaa5eef0bf6b339252e89a128953",
+    "url": "/index.html"
+  }
+];
